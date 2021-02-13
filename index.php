d2986d559d045d2fad987a37dd8fe525506a4186
RodriiTepatzi <72983645+RodriiTepatzi@users.noreply.github.com>
2021-02-12 19:49:23 -0600

    1.0

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body id="body-grid">
+    
+    <nav id="navbar">
+        <ul>
+            <li><a>About</a></li>
+            <li><a>Work</a></li>
+            <li><a>Certifications</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </nav>
+    <div id="welcome-section">
+
+    </div>
+</body>
+
+</html>
